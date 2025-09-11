afde8e806240d6ab5f112b84292eecb708bcdd12
milliepadua <millie.padua@protonmail.com>
2025-09-11 17:39:14 -0600

    First Commit

---
+++ demo0.py
@@ -0,0 +1,44 @@
+x = 3 # Assignment Statement; "Hey CPU, plz allocate memory  for me, and call it x"
+        # in this piece of memory put the value of 3, and if i ever mention x, please give me its value
+
+# function call
+# in terminal you can type: py demo0.py or python demo0.py
+
+y = 4.5 #float
+
+msg = "Hello" #string ; the c language introduced string of characters
+
+name = "Sam" 
+
+
+#George Boole - logic tables = integrated circuits; boolean (True or False)
+old = True
+young = False
+
+print(x)
+print(msg)
+print(name)
+print(y)
+print(old)
+print(young)
+
+#1 - integer, 2 - float, 3 - string, 4 - boolean
+
+#syntax - rules for naming; grammar of the programming language
+#can be letters, underscore, number    ex.  test_1
+#must start with letters but not numbers or underscore
+#cannot have indentation (has meaning)
+#a convention a traditional way of doing something
+#there is a convention for python names of variables
+#building block of programs: statement
+
+
+#naming a variable
+
+v = 3
+u = v == 2
+#this is an equality check v==2, it is a question asking v is equal to the value (becomes true or false)
+#the answer to this question is the value of the expression
+z = 2 + u
+
+print(z)
---
+++ demo1mj.py
@@ -0,0 +1,14 @@
+x = 7
+y = 9.9
+msg = "Peppermint"
+name = "Jack"
+day = True
+night = False
+
+
+print(x)
+print(y)
+print(msg)
+print(name)
+print(day)
+print(night)
