88a9fe7accc54b435b902089709e8e5a0658287c
milliepadua <millie.padua@protonmail.com>
2025-09-18 16:05:44 -0600

    added casting commands

--- week3.py
+++ week3.py
@@ -19,3 +19,21 @@ print("The value of x is: ",x, "and the class type is: ", type(x))
 print("The value of y is: ",y,"and the class type is: ",type(y))
 print("The value of z is: ",z,"and the class type is: ",type(z))
 print("The value of u is: ",u,"and the class type is: ",type(u))
+
+num_as_text="43"
+num_as_num=int(num_as_text)
+
+print(num_as_text)
+print(num_as_num)
+print(str(num_as_num)) #equivalent
+print(num_as_num*2)
+
+num = 3
+num_f = float(3)
+
+num2 = 3.4
+num2_i = int(num2)
+
+num2_as_text = str(num2)
+
+#if you put any variable in the context of a string - automatically it will be a string
